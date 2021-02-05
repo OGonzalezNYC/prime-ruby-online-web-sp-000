351e50b280c3d79af3198ccf5aaf707268711ebc
Omar Gonzalez <ogonzaleznyc@gmail.com>
2021-02-05 12:27:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,19 +1,19 @@
-# def prime?(integer)
-#   if integer > 1  
-#     denominator = 2 
-#     while denominator <= (integer / 2)
-#       if integer % denominator != 0
-#         denominator += 1
-#       else 
-#         return false
-#       end
-#     end
-#     true
-#   else 
-#     false
-#   end
-# end
-
 def prime?(integer)
-  
+  if integer > 1  
+    denominator = 2 
+    while denominator <= (integer / 2)
+      if integer % denominator != 0
+        denominator += 1
+      else 
+        return false
+      end
+    end
+    true
+  else 
+    false
+  end
 end
+
+# def prime?(integer)
+  
+# end
